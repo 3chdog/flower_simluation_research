b1523ba6fb278e3aa9935769311e87a4a1e29018
3chdog <3chdog811038@gmail.com>
2025-02-27 07:44:47 +0800

    Update [quickstart] save models mechanism OK

--- examples/quickstart-pytorch-edit-carefully/pytorchexample/server_app.py
+++ examples/quickstart-pytorch-edit-carefully/pytorchexample/server_app.py
@@ -1,5 +1,8 @@
 """pytorchexample: A Flower / PyTorch app."""
 
+import os
+import datetime
+import numpy as np
 from typing import List, Tuple
 
 from flwr.common import Context, Metrics, ndarrays_to_parameters
@@ -24,9 +27,22 @@ def server_fn(context: Context):
 
     # Read from config
     num_rounds = context.run_config["num-server-rounds"]
+    seed = context.run_config.get("seed", None)
+
+    # Prepare information for model saving
+    info_for_model_saving = {
+        "num_rounds": num_rounds,
+        "model_name": context.run_config["model-name"],
+        "dataset_name": context.run_config["dataset-name"],
+        "seed": seed,
+        "learning_rate": context.run_config["learning-rate"],
+        "saving_folder": "./models/{}".format(datetime.datetime.now().strftime("%Y%m%d-%H%M%S")),
+        "saving_period": context.run_config.get("saving-period", None),
+    }
+    if not os.path.exists(info_for_model_saving["saving_folder"]):
+        os.makedirs(info_for_model_saving["saving_folder"])
 
     # Initialize model parameters
-    seed = context.run_config.get("seed", None)
     if seed is None:
         print("Not set seed, run FL generally.")
     else:
@@ -34,6 +50,19 @@ def server_fn(context: Context):
     ndarrays = get_weights(Net())
     print("\n===  server initial parameters:\n{}\n===\n".format(ndarrays[0].flatten()[:30]))
     parameters = ndarrays_to_parameters(ndarrays)
+
+    # save initial model weights
+    print(f"Saving round #{0} aggregated_ndarrays...")
+    saving_file_name = "{}-{}-num-{}-seed-{}-lr-{}-round-{}-weights.npz".format(
+        info_for_model_saving["dataset_name"],
+        info_for_model_saving["model_name"],
+        "notyet",
+        info_for_model_saving.get("seed", "None"),
+        info_for_model_saving["learning_rate"],
+        0,
+    )
+    saving_file_path = os.path.join(info_for_model_saving["saving_folder"], saving_file_name)
+    np.savez(saving_file_path, *ndarrays)
 
     # Define the strategy
     strategy = FedAvgWithSaving(
@@ -43,6 +72,7 @@ def server_fn(context: Context):
         evaluate_metrics_aggregation_fn=weighted_average,
         initial_parameters=parameters,
         on_fit_config_fn=on_fit_config, # send round number in "config" to clients
+        info_for_model_saving=info_for_model_saving,
     )
     config = ServerConfig(num_rounds=num_rounds)
 
--- examples/quickstart-pytorch-edit-carefully/pytorchexample/strategy.py
+++ examples/quickstart-pytorch-edit-carefully/pytorchexample/strategy.py
@@ -1,3 +1,5 @@
+import os
+import numpy as np
 from typing import Optional, Union
 from flwr.common import (
     FitRes,
@@ -12,9 +14,19 @@ def on_fit_config(server_round: int):
     """Construct `config` that clients receive when running `fit()`"""
     return {"round_num": server_round}
 
+def is_saving(saving_period: Optional[int], server_round: int, num_rounds: int):
+    if saving_period is None:
+        return False
+    if server_round == num_rounds:
+        return True
+    if server_round % saving_period == 0:
+        return True
+    return False
+
 class FedAvgWithSaving(FedAvg):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, info_for_model_saving: dict = {}, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        self.info_for_model_saving = info_for_model_saving
 
     def aggregate_fit(
         self,
@@ -25,10 +37,28 @@ class FedAvgWithSaving(FedAvg):
         """Aggregate the fit results from all clients."""
         parameters_aggregated, metrics_aggregated = super().aggregate_fit(server_round, results, failures)
 
+        # Save aggregated model weights
         aggregated_ndarrays = parameters_to_ndarrays(parameters_aggregated)
         print("\n===  aggregated_ndarrays (round #{}):\n{}\n===\n".format(
             server_round,
             aggregated_ndarrays[0].flatten()[:30])
         )
+        if is_saving(
+            self.info_for_model_saving["saving_period"],
+            server_round,
+            self.info_for_model_saving["num_rounds"]
+        ):
+            print(f"Saving round #{server_round} aggregated_ndarrays...")
+            num_clients = len(results) + len(failures)
+            saving_file_name = "{}-{}-num-{}-seed-{}-lr-{}-round-{}-weights.npz".format(
+                self.info_for_model_saving["dataset_name"],
+                self.info_for_model_saving["model_name"],
+                num_clients,
+                self.info_for_model_saving.get("seed", "None"),
+                self.info_for_model_saving["learning_rate"],
+                server_round,
+            )
+            saving_file_path = os.path.join(self.info_for_model_saving["saving_folder"], saving_file_name)
+            np.savez(saving_file_path, *aggregated_ndarrays)
 
         return parameters_aggregated, metrics_aggregated
