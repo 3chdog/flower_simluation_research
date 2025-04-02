f36d68ee78e88230cebfcdb0a0c12f85a3df9e0a
3chdog <3chdog811038@gmail.com>
2025-04-02 20:43:21 +0800

    Move [quickstart edit] read log and running script to quickstart edit

---
+++ examples/quickstart-pytorch-edit/read_flower_log.py
@@ -0,0 +1,428 @@
+import matplotlib.pyplot as plt
+from typing import Callable
+from dataclasses import dataclass
+
+
+
+@dataclass
+class Metrics:
+    loss: list[float]
+    accuracy: list[float]
+    other_metrics: dict = None
+
+@dataclass
+class Experiment_Result:
+    exp_name: str
+    hyperparameters: dict # check fn: get_all_hyperparamters_from_one_exp_lines()
+    metrics: Metrics
+
+
+
+def get_every_exp_lines(lines: list) -> list[list[str]]:
+    all_exp_lines = []
+    one_exp_lines = []
+    start_flag = False
+    for line in lines:
+        if "[EXPERIMENT #" in line and not start_flag:
+            start_flag = True
+            one_exp_lines = []
+        if start_flag:
+            one_exp_lines.append(line)
+        if "[EXPERIMENT #" in line and "DONE" in line:
+            start_flag = False
+            exp_num = line.split("#")[-1].split("]")[0]
+            # print(f"Experiment {exp_num} end, with {len(one_exp_lines)} lines")
+            all_exp_lines.append(one_exp_lines)
+            one_exp_lines = []
+    return all_exp_lines
+
+def check_type(value, type_assignation: Callable, log_str: str):
+    try:
+        return type_assignation(value)
+    except Exception as e:
+        raise ValueError(
+            "{}\nMaybe type error: Value [{}], its type should be {} but it is {}. Original line:\n{}".format(
+                e, value, type_assignation, type(value), log_str
+            )
+        )
+
+def get_hyperparameter_from_one_exp_lines(
+        one_exp_lines: list,
+        target,
+        exclude: list = ["found in pyproject.toml", "replace"],
+        split_func: Callable = None,
+        type_assignation: Callable = None
+):
+    def no_excluded_words(line) -> bool:
+        for et in exclude:
+            if et in line:
+                return False
+        return True
+
+    # Main Part: find target in one_exp_lines
+    result = None
+    for line in one_exp_lines:
+        if target in line and no_excluded_words(line):
+            result = line
+            break
+
+    # Not Found
+    if result is None:
+        print("Warning: target {} not found in one_exp_lines.\n(Excluded words: {})".format(target, exclude))
+        return None
+
+    # Split
+    if split_func is not None:
+        result = split_func(result)
+
+    # Type Assignation
+    if type_assignation is not None:
+        result = check_type(result, type_assignation, result)
+
+    return result
+
+def get_all_hyperparamters_from_one_exp_lines(one_exp_lines: list):
+    hyperparameters = {
+        "server_rounds": None,
+        "num_clients": None,
+        "epochs": None,
+        "learning_rate": None,
+        "batch_size": None,
+        "hetero": None,
+    }
+    hyperparameters["server_rounds"] = get_hyperparameter_from_one_exp_lines(
+        one_exp_lines=one_exp_lines,
+        target="num-server-rounds",
+        split_func=lambda x: x.split(" = ")[-1],
+        type_assignation=int,
+    )
+    hyperparameters["num_clients"] = get_hyperparameter_from_one_exp_lines(
+        one_exp_lines=one_exp_lines,
+        target="options.num-supernodes", # be aware that there should be only 1 "options.num-supernodes" in the log, originally 2 for cpu and gpu sim mode
+        split_func=lambda x: x.split(" = ")[-1],
+        type_assignation=int,
+    )
+    hyperparameters["epochs"] = get_hyperparameter_from_one_exp_lines(
+        one_exp_lines=one_exp_lines,
+        target="local-epochs",
+        split_func=lambda x: x.split(" = ")[-1],
+        type_assignation=int,
+    )
+    hyperparameters["learning_rate"] = get_hyperparameter_from_one_exp_lines(
+        one_exp_lines=one_exp_lines,
+        target="learning-rate",
+        split_func=lambda x: x.split(" = ")[-1],
+        type_assignation=float,
+    )
+    hyperparameters["batch_size"] = get_hyperparameter_from_one_exp_lines(
+        one_exp_lines=one_exp_lines,
+        target="batch-size",
+        split_func=lambda x: x.split(" = ")[-1],
+        type_assignation=int,
+    )
+    hyperparameters["hetero"] = get_hyperparameter_from_one_exp_lines(
+        one_exp_lines=one_exp_lines,
+        target="hetero",
+        split_func=lambda x: x.split(" = ")[-1],
+        type_assignation=int,
+    )
+    return hyperparameters
+
+def get_losses_from_one_exp_lines(one_exp_lines: list, server_rounds: int):
+    losses = []
+    summary_flag = False
+    for line in one_exp_lines:
+        if "[SUMMARY]" in line:
+            summary_flag = True
+        
+        if summary_flag:
+            if "round " in line:
+                tmp_list = line.split(": ")
+                loss = float(tmp_list[-1])
+                losses.append(loss)
+
+        if "History (metrics, distributed, evaluate):" in line:
+            break
+    assert len(losses) == server_rounds, f"len(losses): {len(losses)}, server_rounds: {server_rounds}"
+    return losses
+
+def get_accuracies_from_one_exp_lines(one_exp_lines: list, server_rounds: int):
+    accuracies = []
+    summary_flag = False
+    for line in one_exp_lines:
+
+        if "History (metrics, distributed, evaluate):" in line:
+            summary_flag = True
+            continue
+        
+        if ")]}" in line and summary_flag:
+            tmp_list = line.split(", ")
+            acc = float(tmp_list[-1][:-3])
+            accuracies.append(acc)
+            break
+        if summary_flag:
+            tmp_list = line.split(", ")
+            acc = float(tmp_list[-1][:-2])
+            accuracies.append(acc)
+    assert len(accuracies) == server_rounds, f"len(accuracies): {len(accuracies)}, server_rounds: {server_rounds}"
+    return accuracies
+
+# generate plots
+def prepare_for_plot(all_exp_results: list[Experiment_Result], metric_name: str, max_show_rnd=None) -> dict[str, list[float]]:
+    experiments = {}
+    for exp in all_exp_results:
+        one_exp_metric = exp.metrics.__getattribute__(metric_name)
+        if max_show_rnd is not None:
+            one_exp_metric = one_exp_metric[:max_show_rnd]
+        experiments[exp.exp_name] = one_exp_metric
+    return experiments
+
+def plot_metric(
+        experiments_1: dict[str, list[float]], # key: exp_name, value: list of metrics (one kind of metrics)
+        experiments_2: dict[str, list[float]], # key: exp_name, value: list of metrics (one kind of metrics)
+        y_label,
+        fig_title,
+        file_name="tmp.jpg",
+):
+    """
+    Plot loss curves for multiple experiments on the same graph.
+    :param experiments: dict, keys are experiment names, values are lists of loss values
+    """
+    plt.figure(figsize=(10, 6))
+    
+    for exp_name, losses in experiments_1.items():
+        plt.plot(range(1, len(losses) + 1), losses, label=exp_name, alpha=0.7, color="red")
+    for exp_name, losses in experiments_2.items():
+        plt.plot(range(1, len(losses) + 1), losses, label=exp_name, alpha=0.7, color="blue")
+    
+    plt.xlabel("Server Rounds")
+    plt.ylabel(y_label)
+    plt.title(fig_title)
+    plt.legend()
+    plt.grid(True, linestyle="--", alpha=0.6)
+    plt.savefig(file_name)
+    plt.close()
+
+def get_exp_results_from_log(log_path: str, mode="original") -> list[Experiment_Result]:
+    # read log
+    with open(log_path, 'r') as f:
+        lines = f.read().split('\n')
+    all_exp_lines = get_every_exp_lines(lines)
+    print("{} exps, every exp lines: {}".format(len(all_exp_lines), [len(one_exp_lines) for one_exp_lines in all_exp_lines]))
+
+    # get all exp results
+    all_exp_results: list[Experiment_Result] = []
+    for i in range(1, len(all_exp_lines) + 1):
+        hyperparameters = get_all_hyperparamters_from_one_exp_lines(all_exp_lines[i-1])
+        num_clients = hyperparameters["num_clients"]
+        lr_str = "0_01" if hyperparameters["learning_rate"]==0.01 else "0_001"
+        losses = get_losses_from_one_exp_lines(all_exp_lines[i-1], server_rounds=hyperparameters["server_rounds"])
+        accuracies = get_accuracies_from_one_exp_lines(all_exp_lines[i-1], server_rounds=hyperparameters["server_rounds"])
+        all_exp_results.append(
+            Experiment_Result(
+                exp_name=f"{num_clients}_clients_lr_{lr_str}_{i}th_{mode}",
+                hyperparameters=hyperparameters,
+                metrics=Metrics(loss=losses, accuracy=accuracies)
+            )
+        )
+    return all_exp_results
+
+def divide_exp_results(all_exp_results: list[Experiment_Result]) -> list[list[Experiment_Result]]:
+    main_group = []
+    results_group = []
+    num_clients = 10
+    learning_rate = 0.01
+    mode = "original"
+    for exp in all_exp_results:
+        if exp.hyperparameters["num_clients"]==num_clients:
+            if exp.hyperparameters["learning_rate"]==learning_rate:
+                if mode in exp.exp_name:
+                    results_group.append(exp)
+    main_group.append(results_group)
+    results_group = []
+    num_clients = 10
+    learning_rate = 0.01
+    mode = "edited"
+    for exp in all_exp_results:
+        if exp.hyperparameters["num_clients"]==num_clients:
+            if exp.hyperparameters["learning_rate"]==learning_rate:
+                if mode in exp.exp_name:
+                    results_group.append(exp)
+    main_group.append(results_group)
+    results_group = []
+    num_clients = 20
+    learning_rate = 0.01
+    mode = "original"
+    for exp in all_exp_results:
+        if exp.hyperparameters["num_clients"]==num_clients:
+            if exp.hyperparameters["learning_rate"]==learning_rate:
+                if mode in exp.exp_name:
+                    results_group.append(exp)
+    main_group.append(results_group)
+    results_group = []
+    num_clients = 20
+    learning_rate = 0.01
+    mode = "edited"
+    for exp in all_exp_results:
+        if exp.hyperparameters["num_clients"]==num_clients:
+            if exp.hyperparameters["learning_rate"]==learning_rate:
+                if mode in exp.exp_name:
+                    results_group.append(exp)
+    main_group.append(results_group)
+    results_group = []
+    num_clients = 50
+    learning_rate = 0.01
+    mode = "original"
+    for exp in all_exp_results:
+        if exp.hyperparameters["num_clients"]==num_clients:
+            if exp.hyperparameters["learning_rate"]==learning_rate:
+                if mode in exp.exp_name:
+                    results_group.append(exp)
+    main_group.append(results_group)
+    results_group = []
+    num_clients = 50
+    learning_rate = 0.01
+    mode = "edited"
+    for exp in all_exp_results:
+        if exp.hyperparameters["num_clients"]==num_clients:
+            if exp.hyperparameters["learning_rate"]==learning_rate:
+                if mode in exp.exp_name:
+                    results_group.append(exp)
+    main_group.append(results_group)
+    results_group = []
+    results_group = []
+    num_clients = 10
+    learning_rate = 0.001
+    mode = "original"
+    for exp in all_exp_results:
+        if exp.hyperparameters["num_clients"]==num_clients:
+            if exp.hyperparameters["learning_rate"]==learning_rate:
+                if mode in exp.exp_name:
+                    results_group.append(exp)
+    main_group.append(results_group)
+    results_group = []
+    num_clients = 10
+    learning_rate = 0.001
+    mode = "edited"
+    for exp in all_exp_results:
+        if exp.hyperparameters["num_clients"]==num_clients:
+            if exp.hyperparameters["learning_rate"]==learning_rate:
+                if mode in exp.exp_name:
+                    results_group.append(exp)
+    main_group.append(results_group)
+    results_group = []
+    num_clients = 20
+    learning_rate = 0.001
+    mode = "original"
+    for exp in all_exp_results:
+        if exp.hyperparameters["num_clients"]==num_clients:
+            if exp.hyperparameters["learning_rate"]==learning_rate:
+                if mode in exp.exp_name:
+                    results_group.append(exp)
+    main_group.append(results_group)
+    results_group = []
+    num_clients = 20
+    learning_rate = 0.001
+    mode = "edited"
+    for exp in all_exp_results:
+        if exp.hyperparameters["num_clients"]==num_clients:
+            if exp.hyperparameters["learning_rate"]==learning_rate:
+                if mode in exp.exp_name:
+                    results_group.append(exp)
+    main_group.append(results_group)
+    results_group = []
+    num_clients = 50
+    learning_rate = 0.001
+    mode = "original"
+    for exp in all_exp_results:
+        if exp.hyperparameters["num_clients"]==num_clients:
+            if exp.hyperparameters["learning_rate"]==learning_rate:
+                if mode in exp.exp_name:
+                    results_group.append(exp)
+    main_group.append(results_group)
+    results_group = []
+    num_clients = 50
+    learning_rate = 0.001
+    mode = "edited"
+    for exp in all_exp_results:
+        if exp.hyperparameters["num_clients"]==num_clients:
+            if exp.hyperparameters["learning_rate"]==learning_rate:
+                if mode in exp.exp_name:
+                    results_group.append(exp)
+    main_group.append(results_group)
+    results_group = []
+    return main_group
+
+def main(list_log_path: list[str]):
+    # read flwr log
+    all_exp_results = []
+    modes = ["original", "edited"]
+    for log_path, mode in zip(list_log_path, modes):
+        all_exp_results += get_exp_results_from_log(log_path, mode)
+    # for exp in all_exp_results: print(exp.exp_name, exp.hyperparameters, len(exp.metrics.loss), len(exp.metrics.accuracy))
+
+    # divide into different groups
+    all_exp_results_divided = divide_exp_results(all_exp_results)
+    for i, group in enumerate(all_exp_results_divided):
+        for exp in group:
+            print(exp.exp_name, exp.hyperparameters, len(exp.metrics.loss), len(exp.metrics.accuracy))
+
+    # start plotting
+    # fig name template: "diff_num_clients_exp_under_same_lr_e_2_iid_500rounds_Loss.jpg"
+    list_lr = ["e_2"]*15 + ["e_3"]*15
+    # list_iid = ["iid"]*10 + ["non_iid"]*10
+    # list_exp_ids = [[0,1,2,3,4,5,6]]*5 + [[7,8,9,10,11,12,13]]*5 + [[14,15,16,17,18,19,20]]*5 + [[21,22,23,24,25,26,27]]*5
+    list_num_clients = [10]*5 + [20]*5 + [50]*5 + [10]*5 + [20]*5 + [50]*5
+    list_max_show_rnd = [5, 10, 20, 100, 500] + [5, 10, 20, 100, 500] + [5, 10, 20, 100, 500] + [5, 10, 20, 100, 500] + [5, 10, 20, 100, 500] + [5, 10, 20, 100, 500]
+
+    # plot Loss
+    titles_loss = ["Loss Curves with lr {} under {} Clients".format(
+        lr, distrib) for lr, distrib in zip(list_lr, list_num_clients)]
+    fig_name_loss = ["exp_charts/lr_{}_{}_clients_{}rounds_Loss.jpg".format(
+        lr, distrib, mx_show_rnd) for lr, distrib, mx_show_rnd in zip(list_lr, list_num_clients, list_max_show_rnd)]
+
+    for i in range(6):
+        exp_ori = all_exp_results_divided[i*2]
+        exp_edit = all_exp_results_divided[i*2+1]
+        for title, fig_name, max_show_rnd in zip(titles_loss[i*5:(i+1)*5], fig_name_loss[i*5:(i+1)*5], list_max_show_rnd[i*5:(i+1)*5]):
+            exp_ori_metric = prepare_for_plot(exp_ori, "loss", max_show_rnd)
+            exp_edit_metric = prepare_for_plot(exp_edit, "loss", max_show_rnd)
+            y_label="Loss"
+            plot_metric(
+                experiments_1=exp_ori_metric,
+                experiments_2=exp_edit_metric,
+                y_label=y_label,
+                fig_title=title,
+                file_name=fig_name,
+            )
+
+    # plot Accuracy
+    titles_acc = ["Accuracy Curves with lr {} under {} Clients".format(
+        lr, distrib) for lr, distrib in zip(list_lr, list_num_clients)]
+    fig_name_acc = ["exp_charts/lr_{}_{}_clients_{}rounds_Accuracy.jpg".format(
+        lr, distrib, mx_show_rnd) for lr, distrib, mx_show_rnd in zip(list_lr, list_num_clients, list_max_show_rnd)]
+
+    for i in range(6):
+        exp_ori = all_exp_results_divided[i*2]
+        exp_edit = all_exp_results_divided[i*2+1]
+        for title, fig_name, max_show_rnd in zip(titles_acc[i*5:(i+1)*5], fig_name_acc[i*5:(i+1)*5], list_max_show_rnd[i*5:(i+1)*5]):
+            exp_ori_metric = prepare_for_plot(exp_ori, "accuracy", max_show_rnd)
+            exp_edit_metric = prepare_for_plot(exp_edit, "accuracy", max_show_rnd)
+            y_label="Accuracy"
+            plot_metric(
+                experiments_1=exp_ori_metric,
+                experiments_2=exp_edit_metric,
+                y_label=y_label,
+                fig_title=title,
+                file_name=fig_name,
+            )
+
+    return all_exp_results
+
+if __name__ == "__main__":
+    main(
+        [
+            "sim_exp_0324_original_quickstart.log",
+            "sim_exp_0324_edited_quickstart.log",
+        ]
+    )
---
+++ examples/quickstart-pytorch-edit/run_fl_exps.sh
@@ -0,0 +1,213 @@
+#################
+### FUNCTIONS ###
+#################
+
+TMP_ARR_BUFFER=()
+
+# function with parameters: original_text, text_to_replace
+replace_text() {
+    sed -i "s/$1/$2/g" pyproject.toml
+}
+
+# search the text in the file
+search_string_in_file() {
+    local string="$1"
+    local file="$2"
+
+    if grep -q "$string" "$file"; then
+        echo -e "\033[0;32mDONE\033[0m String '$string' found in $file"
+        return 0
+    else
+        echo -e "\033[0;31mFAIL\033[0m String '$string' NOT found in $file"
+        return 1
+    fi
+}
+
+# function to make a key merge with an array of values
+generate_array() {
+    local key="$1"
+    shift
+    local values=("$@")
+    local list=()
+    for value in "${values[@]}"; do
+        list+=("${key} = ${value}")
+    done
+
+    TMP_ARR_BUFFER=()
+    TMP_ARR_BUFFER+=("${list[@]}")  # Store the generated array in a temporary buffer
+}
+
+# merge and then append
+merge_to_string() {
+    local prefix="$1"
+    shift  # Remove the first argument (prefix)
+    local list=$(generate_array ${prefix} "$@")
+    
+    local result_string="$prefix: "
+    for part in "${list[@]}"; do
+        result_string+="$part "
+    done
+
+    # Trim the trailing space and return the string
+    echo "${result_string% }"
+}
+
+# Function to convert a string into an array and output it
+convert_string_to_array() {
+    local string="$1"
+    # Extract the part after the first colon and split by space into the array
+    IFS=' ' read -r -a temp_array <<< "${string#*:}"
+    
+    # Output the array (space-separated)
+    # Get the result by array_result=($(convert_string_to_array "array1:value1a value1b value1c"))
+    echo "${temp_array[@]}"
+}
+
+
+#############################
+### EXPERIMENT PARAMETERS ###
+#############################
+# IMPORTANT: enter 1. the number of experiments to run 2. the number of arrays of paramters you set
+echo "[HYPERPARAMETERS SETTING]"
+n=3
+num_arrays=5
+pyproject_file="pyproject.toml"
+# IMPORTANT: edit p_key, p_value, and the name of the variable like p_arr{i} for each parameter
+
+# number of clients
+p_key="options.num-supernodes"
+p_value=(\
+4 \
+4 \
+4 \
+)
+search_string_in_file "${p_key} =" "${pyproject_file}" || exit 1
+p_original=$(cat "${pyproject_file}" | grep "${p_key} = [0-9]\+")
+generate_array "${p_key}" "${p_value[@]}"
+p_arr1+=("${p_original}" "${TMP_ARR_BUFFER[@]}")
+
+# number of rounds
+p_key="num-server-rounds"
+p_value=(\
+3 \
+3 \
+3 \
+)
+search_string_in_file "${p_key} =" "${pyproject_file}" || exit 1
+p_original=$(cat "${pyproject_file}" | grep "${p_key} = [0-9]\+")
+generate_array "${p_key}" "${p_value[@]}"
+p_arr2+=("${p_original}" "${TMP_ARR_BUFFER[@]}")
+
+# learning rate
+p_key="learning-rate"
+p_value=(\
+0.01 \
+0.01 \
+0.01 \
+)
+search_string_in_file "${p_key} =" "${pyproject_file}" || exit 1
+p_original=$(cat "${pyproject_file}" | grep "${p_key} = [0-9]\+")
+generate_array "${p_key}" "${p_value[@]}"
+p_arr3+=("${p_original}" "${TMP_ARR_BUFFER[@]}")
+
+# hetero
+p_key="hetero"
+p_value=(\
+0 \
+0 \
+0 \
+)
+search_string_in_file "${p_key} =" "${pyproject_file}" || exit 1
+p_original=$(cat "${pyproject_file}" | grep "${p_key} = [0-9]\+")
+generate_array "${p_key}" "${p_value[@]}"
+p_arr4+=("${p_original}" "${TMP_ARR_BUFFER[@]}")
+
+# seeds
+p_key="seed"
+for ((i=0; i<n; i++)); do
+    p_value[i]=$RANDOM
+done
+search_string_in_file "${p_key} =" "${pyproject_file}" || exit 1
+p_original=$(cat "${pyproject_file}" | grep "${p_key} = [0-9]\+")
+generate_array "${p_key}" "${p_value[@]}"
+p_arr5+=("${p_original}" "${TMP_ARR_BUFFER[@]}")
+
+
+###########################
+### EXPERIMENT CHECKING ###
+###########################
+
+echo "[CHECKING HYPERPARAMETERS]"
+echo "Number of experiments: $n"
+echo "Kinds of parameters: $num_arrays"
+
+# check the number of parameters match n
+for ((i=0; i<num_arrays; i++)); do
+    array_name="p_arr$((i+1))"
+    eval "current_array=(\"\${$array_name[@]}\")"
+
+    arr_length=${#current_array[@]}
+    current_array_length=${#current_array[@]}
+    if [ $(($current_array_length-1)) -lt $n ]; then
+        echo -e "\033[0;31mFAIL\033[0m len(${array_name})=$(($arr_length-1)) is less than n=${n}. Please check the parameters."
+        exit 1
+    elif [ $(($current_array_length-1)) -gt $n ]; then
+        echo -e "\033[0;33mWARNING\033[0m len(${array_name})=$(($arr_length-1)) is greater than n=${n}. Keep going and some of the parameters will not be set in experiments."
+    else
+        continue
+    fi
+done
+
+# dry run
+echo "[DRY RUN]"
+for i in $(seq 1 $n); do
+    # check previous hyperparameters exist for each array (p_arr1, p_arr2...)
+    for ((j=1; j<=num_arrays; j++)); do
+        array_name="p_arr$j"
+        eval "current_array=(\"\${$array_name[@]}\")"
+        search_string_in_file "${current_array[$i-1]}" pyproject.toml || $(echo "Failed in EXPERIMENT #$i"; exit 1)
+    done
+
+    # test replacing the hyperparameters for each array (p_arr1, p_arr2...)
+    for ((j=1; j<=num_arrays; j++)); do
+        array_name="p_arr$j"
+        eval "current_array=(\"\${$array_name[@]}\")"
+        # replace the hyperparameters
+        replace_text "${current_array[$i-1]}" "${current_array[$i]}"
+        echo "replace ${current_array[$i-1]} -> ${current_array[$i]}"
+    done
+done
+
+# recover the original pyproject.toml for each array (p_arr1, p_arr2...)
+for ((j=1; j<=num_arrays; j++)); do
+    array_name="p_arr$j"
+    eval "current_array=(\"\${$array_name[@]}\")"
+    # replace the hyperparameters
+    replace_text "${current_array[$n]}" "${current_array[0]}"
+    echo "recover ${current_array[$n]} -> ${current_array[0]}"
+done
+echo "[DRY RUN] DONE"
+
+#######################
+### RUN EXPERIMENTS ###
+#######################
+
+# doing the experiments (1~n, including n)
+for i in $(seq 1 $n); do
+    echo -e "\n===\n[EXPERIMENT #$i]\n"
+
+    echo -e "[Replace Config]"
+    for ((j=1; j<=num_arrays; j++)); do
+        array_name="p_arr$j"
+        eval "current_array=(\"\${$array_name[@]}\")"
+        # replace the hyperparameters
+        replace_text "${current_array[$i-1]}" "${current_array[$i]}"
+        echo "replace ${current_array[$i-1]} -> ${current_array[$i]}"
+    done
+
+    # run the experiment
+    cat pyproject.toml
+    flwr run .
+    # end
+    echo -e "\n[EXPERIMENT #$i] DONE\n===\n"
+done
